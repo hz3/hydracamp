0f66febc009dbd39103d6821d37673f531560a28
huizhang <huizhang@huizhang-studio.(none)>
2012-10-08 14:21:54 -0400

    first comit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hydracamp::Application.config.secret_token = 'fea4b8edf9003a3b46b25d004281925b850f1284275d217406e2b228fe4b92ed45f009c470696f843296ca026d873b774252a41bf1651c8598012ea83d88ba2b'
